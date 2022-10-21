bd295e91a2b763497df7c2524f8fbdb51177c3e4
Hadi Keramati <37190613+hkeramat@users.noreply.github.com>
2022-10-21 12:47:27 -0400

    Create 17. Letter Combinations of a Phone Number_BFS.py

---
+++ 17. Letter Combinations of a Phone Number_BFS.py
@@ -0,0 +1,21 @@
+from collections import deque
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if not digits:
+            return 
+        
+        letters = {"2": "abc", "3": "def", "4": "ghi", "5": "jkl", 
+                   "6": "mno", "7": "pqrs", "8": "tuv", "9": "wxyz"}
+        
+        q =deque(letters[digits[0]])
+        
+        for i in range(1, len(digits)):
+            s = len(q)
+            while s:
+                out = q.popleft()
+                for j in letters[digits[i]]:
+                    q.append(out+j)
+                s-=1
+                
+        return q
+        
